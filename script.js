7c6baca74e071d8089a19f49cd5f84ab1558b6d4
Your name here <kr.shampa@yahoo.com>
2020-04-23 14:30:37 -0500

    adding video

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 'use strict';
 
 
- 
+
 function getRecipe(searchTerm) {
     const url = `https://yummly2.p.rapidapi.com/feeds/search?maxTotalTimeInSeconds=7200&q=${searchTerm}%20soup&start=0&maxResult=8`;
     console.log(url);
-    fetch(url, 
-    {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "yummly2.p.rapidapi.com",
-            "x-rapidapi-key": "LgHxMTYKPpmshwhb6BYsVrPRtpvXp1pbPh5jsnDRyaXptvxh43"
-        }
-    })
-        
+    fetch(url,
+        {
+            "method": "GET",
+            "headers": {
+                "x-rapidapi-host": "yummly2.p.rapidapi.com",
+                "x-rapidapi-key": "LgHxMTYKPpmshwhb6BYsVrPRtpvXp1pbPh5jsnDRyaXptvxh43"
+            }
+        })
+
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -25,36 +25,40 @@ function getRecipe(searchTerm) {
             $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
 }
-function displayRecipe(responseJson){
+function displayRecipe(responseJson) {
     //console.log(responseJson);
-    const feeds= responseJson.feed;
+    const feeds = responseJson.feed;
     $('#results-list').empty();
-   $('#results').removeClass('hidden');
-   
-   
-   console.log(feeds[0].display.displayName);
-    const contentFeed = responseJson.feed.content;
-    console.log(feed[0].content.videos.originalVideoUrl)
-    /* if(!(results.totalResults == 0))
-    { */
-        
-         /* for(let i =0; i<feeds.length; i++)
-        {
-       // console.log(feeds[i].images[0]);
+    $('#results').removeClass('hidden');
+
+
+    console.log(feeds[0].display.displayName);
+    //console.log(feeds[0].console.preparationSteps[0]);
+    console.log(feeds[0].content.videos.originalVideoUrl)
+    /*  if(!(results.totalResults == 0)) */
+    /* {  */
+
+    for (let i = 0; i < feeds.length; i++) {
+        //console.log(feeds[i].console.preparationSteps);
         console.log(feeds[i].display.displayName);
         console.log(feeds[i].content.videos.originalVideoUrl);
-        
-        const entry= `<li>
+
+        const entry = `<li>
                <h3> ${feeds[i].display.displayName}</h3>
-               
+               var video = $('<embed />', {
+                        id: 'video',
+                        src: feeds[i].content.videos.originalVideoUrl,
+                        type: 'mp4',
+                        controls: true
+                        }); 
         </li> `
-        $('#results-list').append(entry); */
-   /*   } */ 
-     
-    } 
+        $('#results-list').append(entry);
+    }
+
+    /* }  */
     /* else $('#results-list').html("Recipe not found");  */
 
- /* } */ 
+}
 
 function watchForm() {
     $('form').submit(event => {
